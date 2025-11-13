950201979b3cfe686f3b3cc2c13b9abc9527f197
Kabz Jose <josekabz00@gmail.com>
2025-11-13 22:15:26 +0300

    Usestate in Counter app

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 
 import Header from "./Header";
-//import Card from "./card";
 import Footer from "./footer";
-import Student from "./student";
+import Counter from "./student";
 function App(){
   return (
     <><>
     <Header/>
-    <Student name="John"age="19" bio="A passionate sodtware engineer and a data annotator"/>
-    <Student name="Sharon"age="21" bio="a pasiionate jounalist"/>
-   <Student name="Dan"age="23" bio="A passionate drummer"/>
+    <Counter/>
    
    
   <Footer/>
--- src/student.jsx
+++ src/student.jsx
@@ -1,11 +1,15 @@
-function Student({name,age,bio}){
-    
+import  { useState } from "react";
+function Counter({}){
+    const [count,setCount]=useState(0);
+    //count=current value
+    //setCount= function to update the count value
     return(
        <div>
-        <h2>Name:{name}</h2>
-        <p>Age: {age}</p>
-        <p>Proffession: {bio}</p>
+        <h2>count: {count}</h2>
+        <button onClick={()=>setCount(count +1)}>Increase</button>
+        <button onClick={()=>setCount(count -1)}>Decrease</button>
+        <button onClick={()=> setCount(0)}>Reset</button>
        </div>
     )
 }
-export default Student;
+export default Counter;
